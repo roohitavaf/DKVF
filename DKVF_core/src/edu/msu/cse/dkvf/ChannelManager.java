a616376a5c18ee607dc17df50cc13262fd1f0f4b
Mohammad <roohitavaf@gmail.com>
2018-01-07 13:20:01 -0500

    adding dkvf_core

---
+++ DKVF_core/src/edu/msu/cse/dkvf/ChannelManager.java
@@ -0,0 +1,113 @@
+package edu.msu.cse.dkvf;
+
+
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.text.MessageFormat;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.logging.Logger;
+
+import com.google.protobuf.CodedOutputStream;
+
+import edu.msu.cse.dkvf.metadata.Metadata.ServerMessage;
+/**
+ * This class manages the reliable FIFO delivery to servers.
+ *
+ */
+public class ChannelManager implements Runnable {
+	LinkedBlockingDeque<ServerMessage> deque = new LinkedBlockingDeque<>();
+	boolean running = true;
+
+	private Socket socket;
+	private CodedOutputStream out;
+	private String ip;
+	private int port;
+	private ServerMessage currentMessage;
+	private int tryAgainWaitTime;
+	private Logger logger;
+	
+	/**
+	 * Constructor for ChannelManager class
+	 * @param ip The IP address of the destination
+	 * @param port The port number of the destination
+	 * @param tryAgainWaitTime The time before trying again in case of a failed delivery
+	 * @param capacity The capacity of pending messages
+	 * @param logger The logger
+	 */
+	public ChannelManager(String ip, int port, int tryAgainWaitTime, int capacity, Logger logger) {
+		this.ip = ip;
+		this.port = port;
+		this.logger = logger;
+		this.deque = new LinkedBlockingDeque<>(capacity);
+		this.tryAgainWaitTime = tryAgainWaitTime;
+		Thread thread = new Thread(this);
+		thread.start();
+		
+	}
+
+	/**
+	 * Connects to the destination
+	 * 
+	 * @throws UnknownHostException
+	 * @throws IOException
+	 */
+	public void connect() throws UnknownHostException, IOException {
+		socket = new Socket(ip, port);
+		out = CodedOutputStream.newInstance(socket.getOutputStream());
+	}
+
+	/**
+	 * Starts sending messages to the destination
+	 */
+	public void run() {
+		boolean connectionFailure = false;
+		while (running) {
+			try {
+				if (connectionFailure){
+					if (socket != null)
+						socket.close();
+					connectionFailure = false;
+				}
+				if (socket == null || socket.isClosed() || !socket.isConnected())
+					connect();
+				
+				if (currentMessage == null)
+					currentMessage = deque.takeFirst();
+				logger.finest(MessageFormat.format("Sending message to ip: {0}, port:{1}\n Message:\n{2}", ip, port, currentMessage.toString()));
+				out.writeInt32NoTag(currentMessage.getSerializedSize());
+				currentMessage.writeTo(out);
+				out.flush();
+				currentMessage = null;
+			} catch (InterruptedException e) {
+				// TODO log here: Interrupted while waiting for queue.
+			}catch (UnknownHostException e){
+				//just log and exit
+				break;
+			}catch (IOException e) {
+				// TODO log here
+				try {
+					Thread.sleep(tryAgainWaitTime);
+					connectionFailure = true;
+				} catch (InterruptedException e1) {
+					// TODO log here: Interrupted while waiting to try again. 
+				}
+			}
+		}
+	}
+
+	/** 
+	 * Adds a new server message to the queue. 
+	 * @param sm The ServerMessage object to add to the queue. 
+	 */
+	public void addMessage(ServerMessage sm) {
+		deque.addLast(sm);
+	}
+
+	/**
+	 * Stops sending messages.
+	 */
+	public void stop() {
+		running = false;
+	}
+}
---
+++ DKVF_core/src/edu/msu/cse/dkvf/ServerHandler.java
@@ -0,0 +1,63 @@
+package edu.msu.cse.dkvf;
+
+import java.net.Socket;
+import java.net.SocketException;
+import java.text.MessageFormat;
+import java.util.logging.Logger;
+
+import com.google.protobuf.CodedInputStream;
+
+import edu.msu.cse.dkvf.metadata.Metadata.ServerMessage;
+
+/** 
+ * Server handler class
+ *
+ */
+public class ServerHandler implements Runnable {
+
+	/**
+	 * The protocol to run its server handler upon receiving a server message
+	 */
+	DKVFServer protocol;
+	
+	Socket clientSocket;
+	Logger LOGGER;
+
+	/** 
+	 * Calls the protocol {@link DKVFServer#handleServerMessage} upon receiving a server message. 
+	 */
+	public ServerHandler(Socket clientSocket, DKVFServer protocol, Logger logger) {
+		LOGGER = logger;
+		this.clientSocket = clientSocket;
+		this.protocol = protocol;
+	}
+
+	@Override
+	/**
+	 * Handles server message. It calls the protocol
+	 * {@link edu.msu.cse.dkvf.DKVFServer#handleServerMessage} upon
+	 * receiving a server message.
+	 */
+	public void run() {
+		try {
+			CodedInputStream in = CodedInputStream.newInstance(clientSocket.getInputStream());
+			while (!clientSocket.isClosed()) {
+				int size = in.readInt32();
+				byte[] newMessageBytes = in.readRawBytes(size);
+				ServerMessage sm = ServerMessage.parseFrom(newMessageBytes);
+				if (sm == null)
+					return;
+				LOGGER.finest(MessageFormat.format("New server message arrived:\n{0}", sm.toString()));
+				protocol.handleServerMessage(sm);
+			}
+		} catch (SocketException e) {
+			LOGGER.info("Socket exception in server handler. Probably server has closed the socket.");
+			protocol.decrementNumberOfServers();
+		} catch (Exception e) {
+			LOGGER.severe(MessageFormat.format("Error in reading server message. toString: {0} Message:\n{1}",
+					e.toString(), e.getMessage()));
+		}
+
+	}
+
+}
